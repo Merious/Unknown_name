bdc78c7db32bc453a36cb398dd0d2e2f23761617
Mer1ous <mi.chekmarev@gmail.com>
2023-08-15 17:11:37 +0300

    lesson1

--- Untitled-1.py
+++ Untitled-1.py
@@ -1,22 +1,6 @@
-def strcounter(s):
-    for sym in s:
-        counter = 0
-        for sub_sym in s:
-            if sub_sym == sym:
-                counter += 1
-        print(sym, counter)
+stroka = str(input())
 
-def strcounter2(s):
-    for sym in set(s):
-        counter = 0
-        for sub_sym in s:
-            if sub_sym == sym:
-                counter += 1
-        print(sym, counter)
-
-def strcounter3(s):
-    syms_counter = {}
-    for sym in s:
-        syms_counter[sym] = syms_counter.get(sym, 0) + 1
-
-strcounter3('aadyyywn')
+if stroka == stroka[::-1]:
+  print("true")
+else:
+  print("false")
